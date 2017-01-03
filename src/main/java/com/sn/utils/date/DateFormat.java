f1a3232ebafe103e6ba2c7f3478e60c052b0ef69
snzigod <snzigod@hotmail.com>
2017-01-03 10:43:44 +0800

    日期处理工具

---
+++ src/main/java/com/sn/utils/date/DateFormat.java
@@ -0,0 +1,31 @@
+package com.sn.utils.date;
+
+/**
+ * 日期格式化
+ */
+public abstract class DateFormat {
+	// 年月日
+	public final static String YYYYMMDD = "yyyyMMdd";
+	public final static String YYYYMMDD_EN = "yyyy-MM-dd";
+	public final static String YYYYMMDD_CN = "yyyy年MM月dd日";
+	// 年月日时
+	public final static String YYYYMMDDHH24 = "yyyyMMddHH";
+	public final static String YYYYMMDDHH24_EN = "yyyy-MM-dd HH";
+	public final static String YYYYMMDDHH24_CN = "yyyy年MM月dd日HH时";
+	// 年月日时分
+	public final static String YYYYMMDDHH24MM = "yyyyMMddHHmm";
+	public final static String YYYYMMDDHH24MM_EN = "yyyy-MM-dd HH:mm";
+	public final static String YYYYMMDDHH24MM_CN = "yyyy年MM月dd日HH时mm分";
+	// 年月日时分秒
+	public final static String YYYYMMDDHH24MMSS = "yyyyMMddHHmmss";
+	public final static String YYYYMMDDHH24MMSS_EN = "yyyy-MM-dd HH:mm:ss";
+	public final static String YYYYMMDDHH24MMSS_CN = "yyyy年MM月dd日HH时mm分ss秒";
+	// 时分
+	public final static String HH24MM = "HHmm";
+	public final static String HH24MM_EN = "HH:mm";
+	public final static String HH24MM_CN = "HH时mm分";
+	// 时分秒
+	public final static String HH24MMSS = "HHmmss";
+	public final static String HH24MMSS_EN = "HH:mm:ss";
+	public final static String HH24MMSS_CN = "HH时mm分ss秒";
+}
